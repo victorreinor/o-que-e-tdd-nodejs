4ae1f1ea43ef223611d4aebb6f05aef9ef7f7e36
Victor Reinor <victorreinor@gmail.com>
2019-10-29 20:21:46 -0300

    adiciona funcao que gera registros aleatorios

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -2,7 +2,7 @@ import request from 'supertest';
 import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 
-import User from '../../src/app/models/User';
+import factory from '../factories';
 import truncate from '../util/truncate';
 
 describe('User', () => {
@@ -11,11 +11,7 @@ describe('User', () => {
   });
 
   it('should encrypt user password when new user created', async () => {
-    const user = await User.create({
-      name: 'Victor Reinor',
-      email: 'victorreinor@gmail.com',
-      password: '123456',
-    });
+    const user = await factory.create('User', { password: '123456' });
 
     const compareHash = await bcrypt.compare('123456', user.password_hash);
 
@@ -23,33 +19,25 @@ describe('User', () => {
   });
 
   it('should be able to register', async () => {
+    const user = await factory.attrs('User');
+
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Victor Reinor',
-        email: 'victorreinor@gmail.com',
-        password: '123456',
-      });
+      .send(user);
 
     expect(response.body).toHaveProperty('id');
   });
 
   it('should not be able to register with duplicated email', async () => {
+    const user = await factory.attrs('User');
+
     await request(app)
       .post('/users')
-      .send({
-        name: 'Victor Reinor',
-        email: 'victorreinor@gmail.com',
-        password: '123456',
-      });
+      .send(user);
 
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Victor Reinor',
-        email: 'victorreinor@gmail.com',
-        password: '123456',
-      });
+      .send(user);
 
     expect(response.status).toBe(400);
   });
