46aff0cb51a8fc2194544f5bb58852d024cee1c9
Victor Reinor <victorreinor@gmail.com>
2019-10-29 19:28:18 -0300

    adiciona test de email duplicado

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,7 +1,13 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
@@ -22,8 +28,6 @@ describe('User', () => {
         email: 'victorreinor@gmail.com',
         password_hash: '123456',
       });
-
-    expect(response.body).toHaveProperty('id');
 
     const response = await request(app)
       .post('/users')
--- src/database/migrations/20191022215844-create-users.js
+++ src/database/migrations/20191022215844-create-users.js
@@ -1,32 +1,33 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.createTable('users', {
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
-    },
-    name: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    password_hash: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    created_at: {
-      type: Sequelize.DATE,
-      allowNull: false,
-    },
-    updated_at: {
-      type: Sequelize.DATE,
-      allowNull: false,
-    },
-  }),
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    }),
 
-  down: (queryInterface) => queryInterface.dropTable('users'),
+  down: queryInterface => queryInterface.dropTable('users'),
 };
